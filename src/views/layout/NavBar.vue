044e83a89ab63babfdca5dd31b2212bd86d0004d
Orange <106434581+Fjsrange@users.noreply.github.com>
2024-05-27 00:03:43 +0800

    前端乐哥，p31-p33，静态数据和渲染，index可设置为path

--- src/views/layout/NavBar.vue
+++ src/views/layout/NavBar.vue
@@ -4,6 +4,12 @@
       collapse="true"
 
      -->
+    <h1 class="main-logo">
+      <!-- <img src="../../assets/logo.png" v-if="isCollapse" width="32" />
+      <span v-else>通用后台管理系统</span> -->
+      <img src="../../assets/logo.png" width="32" />
+      <span v-show="!isCollapse">通用后台管理系统</span>
+    </h1>
     <el-menu
       default-active="2"
       class="el-menu-vertical-demo"
@@ -16,26 +22,35 @@
       text-color="#fff"
       active-text-color="##29b8db"
     >
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航一</span>
-        </template>
-        <el-menu-item index="1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
-      <el-submenu index="3">
+      <div v-for="(item, index) in menuData" :key="index">
+        <el-submenu :index="`${index + 1}`" v-if="item.children">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>{{ item.title }}</span>
+          </template>
+          <el-menu-item
+            :index="`${index + 1}-${ind + 1}`"
+            v-for="(chi, ind) in item.children"
+            :key="ind"
+          >
+            {{ chi.title }}</el-menu-item
+          >
+          <!-- <el-menu-item index="1-2">选项2</el-menu-item> -->
+        </el-submenu>
+        <el-menu-item :index="`${index + 1}`" v-else>
+          <i class="el-icon-menu"></i>
+          <span slot="title">{{ item.title }}</span>
+        </el-menu-item>
+      </div>
+
+      <!-- <el-submenu index="3">
         <template slot="title">
           <i class="el-icon-menu"></i>
           <span>导航三</span>
         </template>
         <el-menu-item index="3-1">选项1</el-menu-item>
         <el-menu-item index="3-2">选项2</el-menu-item>
-      </el-submenu>
+      </el-submenu> -->
     </el-menu>
   </div>
 </template>
@@ -47,7 +62,39 @@ export default {
   name: 'NavBar',
   components: {},
   data () {
-    return {}
+    return {
+      menuData: [
+        {
+          title: '首页',
+          path: '/'
+        },
+        {
+          title: '客户管理',
+          children: [
+            {
+              title: '客户列表'
+            },
+            {
+              title: '客户详情'
+            }
+          ]
+        },
+        {
+          title: '订单管理',
+          children: [
+            {
+              title: '订单列表'
+            },
+            {
+              title: '订单详情'
+            }
+          ]
+        },
+        {
+          title: '流程管理'
+        }
+      ]
+    }
   },
   computed: {
     ...mapGetters(['isCollapse']) // 折叠导航栏
@@ -71,6 +118,23 @@ export default {
   box-shadow: 5px 0 5px #ccc;
   // 设置所有元素的过渡时间为0.3秒
   transition: all 0.3s;
+
+  img {
+    vertical-align: middle;
+  }
+  .main-logo {
+    padding: 10px 16px;
+
+    span {
+      min-width: 130px;
+      color: #fff;
+      font-weight: bold;
+      display: inline-block;
+      position: absolute;
+      left: 62px;
+      top: 16px;
+    }
+  }
 
   .el-menu {
     border: none;
